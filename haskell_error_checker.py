a5ed9ebb64d800310a712d5c5815e635e5fe81ee
Keith Holman <holmak@gmail.com>
2012-02-25 17:49:03 -0800

    The write_output() method now writes the specified string instead of a hardcoded string.

--- haskell_error_checker.py
+++ haskell_error_checker.py
@@ -11,6 +11,6 @@ class HaskellErrorChecker(sublime_plugin.EventListener):
         PANEL_NAME = 'haskell_error_checker'
         output_view = view.window().get_output_panel(PANEL_NAME)
         edit = output_view.begin_edit()
-        output_view.insert(edit, 0, 'You have errors in your program!')
+        output_view.insert(edit, 0, text)
         output_view.end_edit(edit)
         view.window().run_command('show_panel', {'panel': 'output.' + PANEL_NAME})
