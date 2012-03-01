5842b45a7eb407cc49290272aab4f135512c849e
Keith Holman <holmak@gmail.com>
2012-03-01 00:16:36 -0800

    Error messages are now parsed and displayed with each on a single line.

--- haskell_error_checker.py
+++ haskell_error_checker.py
@@ -1,8 +1,16 @@
 import fnmatch
 import os
+import re
 import sublime
 import sublime_plugin
 import subprocess
+
+# This regex matches an unindented line, followed by zero or more
+# indented, non-empty lines.
+# The first line is divided into a filename, a line number, and a column.
+error_output_regex = re.compile(
+    '^(\S*):(\d+):(\d+):(.*$(?:\n^[ \t].*$)*)',
+    re.MULTILINE)
 
 class HaskellErrorChecker(sublime_plugin.EventListener):
     def on_post_save(self, view):
@@ -15,7 +23,8 @@ class HaskellErrorChecker(sublime_plugin.EventListener):
         if is_haskell_file and cabal_file_path is not None:
             project_dir = os.path.dirname(cabal_file_path)
             error_messages = cabal_build(project_dir)
-            self.write_output(view, error_messages)
+            error_messages_as_string = '\n'.join([str(x) for x in error_messages])
+            self.write_output(view, error_messages_as_string)
 
     def write_output(self, view, text):
         PANEL_NAME = 'haskell_error_checker'
@@ -24,6 +33,21 @@ class HaskellErrorChecker(sublime_plugin.EventListener):
         output_view.insert(edit, 0, text)
         output_view.end_edit(edit)
         view.window().run_command('show_panel', {'panel': 'output.' + PANEL_NAME})
+
+class ErrorMessage(object):
+    "Describe an error or warning message produced by GHC."
+    def __init__(self, filename, line, column, message):
+        self.filename = filename
+        self.line = line
+        self.column = column
+        self.message = message
+
+    def __str__(self):
+        return '{0}: line {1}, column {2}: {3}'.format(
+            self.filename,
+            self.line,
+            self.column,
+            self.message)
 
 def does_view_contain_haskell_source(view):
     "Return True if the specified view is displaying Haskell source code."
@@ -65,7 +89,27 @@ def cabal_build(dir):
         stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     exit_code = process.wait()
-    return stderr
+    return parse_error_messages(stderr)
+
+def parse_error_messages(text):
+    "Parse text into a list of ErrorMessage objects."
+    matches = error_output_regex.finditer(text)
+    messages = []
+    for m in matches:
+        filename, line, column, messy_details = m.groups()
+        messages.append(ErrorMessage(
+            filename,
+            line,
+            column,
+            clean_whitespace(messy_details)))
+    return messages
+
+def clean_whitespace(text):
+    """Remove leading and trailing whitespace, plus replaces any interior 
+    whitespace with a single space."""
+    text = text.strip()
+    text = re.sub('\s+', ' ', text)
+    return text
 
 def log(message):
     print('[hs-errcheck] ' + message)
