7ef32d0ef5ea88cca5a4d895162cc447e4181c80
Keith Holman <holmak@gmail.com>
2012-03-09 22:23:30 -0800

    Moved common functions into the common python module.

--- autobuild.py
+++ autobuild.py
@@ -122,45 +122,6 @@ def write_output(view, text, cabal_project_dir):
     # Show the results panel:
     view.window().run_command('show_panel', {'panel': 'output.' + PANEL_NAME})
 
-def get_cabal_project_dir_of_view(view):
-    """Return the path to the .cabal file project for the source file in the
-    specified view. The view must show a saved file, the file must be Haskell
-    source code, and the file must be under a directory containing a .cabal file.
-    Otherwise, return None.
-    """
-    # Check that the view is showing a saved file:
-    file_shown_in_view = view.file_name()
-    if file_shown_in_view is None:
-        return None
-    # Check that the file is Haskell source code:
-    syntax_file_for_view = view.settings().get('syntax').lower()
-    if  'haskell' not in syntax_file_for_view:
-        return None
-    # Check that a .cabal file is present:
-    cabal_file_path = find_file_in_parent_dir(
-        os.path.dirname(file_shown_in_view), '*.cabal')
-    # Return the directory containing the .cabal file:
-    return os.path.dirname(cabal_file_path)
-
-def find_file_in_parent_dir(subdirectory, filename_pattern):
-    """Look for a file with the specified name in a parent directory of the
-    specified directory. If found, return the file's full path. Otherwise,
-    return None."""
-    current_dir = subdirectory
-    while True:
-        # See if the current directory contains the desired file:
-        for name in os.listdir(current_dir):
-            full_path = os.path.join(current_dir, name)
-            matches_pattern = fnmatch.fnmatch(name, filename_pattern)
-            if matches_pattern and os.path.isfile(full_path):
-                return full_path
-        # Get the next directory up:
-        last_dir = current_dir
-        current_dir = os.path.dirname(current_dir)
-        # Check to see if we have reached the root directory:
-        if last_dir == current_dir:
-            return None
-
 def parse_error_messages(text):
     "Parse text into a list of ErrorMessage objects."
     matches = error_output_regex.finditer(text)
