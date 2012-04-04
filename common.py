9eb4defee4aa561eba18ec4047cc49ad7b70f8fe
Luke Iannini <lukexi@me.com>
2012-04-03 22:27:11 -0700

    Added subprocess.mswindows check to call_and_wait STARTUPINFO configuration

--- common.py
+++ common.py
@@ -10,13 +10,14 @@ def call_and_wait(command, **popen_kwargs):
     """Run the specified command, block until it completes, and return
     the exit code, stdout, and stderr.
     Additional parameters to Popen can be specified as keyword parameters."""
-    startupinfo = subprocess.STARTUPINFO()
-    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    if subprocess.mswindows:
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        popen_kwargs['startupinfo'] = startupinfo
     process = subprocess.Popen(
         command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        startupinfo=startupinfo,
         **popen_kwargs)
     stdout, stderr = process.communicate()
     exit_code = process.wait()
