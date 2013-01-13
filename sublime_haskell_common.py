3fc4a8d790b59cdbc1e5fd7463d2454bf1ad3cf4
Niklas Hambüchen <mail@nh2.me>
2013-01-13 11:00:05 +0100

    Log ghc-mod invocations

--- sublime_haskell_common.py
+++ sublime_haskell_common.py
@@ -247,10 +247,11 @@ def call_ghcmod_and_wait(arg_list, filename=None):
     ghc_cwd = (get_cabal_project_dir_of_file(filename) or os.path.dirname(filename)) if filename else None
 
     try:
-        exit_code, out, err = call_and_wait(
-            try_attach_sandbox(['ghc-mod'] + arg_list),
-            cwd=(ghc_cwd or os.getcwd())
-        )
+        command = try_attach_sandbox(['ghc-mod'] + arg_list)
+
+        log('running ghc-mod: {0}'.format(command))
+
+        exit_code, out, err = call_and_wait(command, cwd=(ghc_cwd or os.getcwd()))
 
         if exit_code != 0:
             raise Exception("ghc-mod exited with status %d and stderr: %s" % (exit_code, err))
