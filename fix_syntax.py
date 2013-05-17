98d1f3d83f2ceb2cf48a6819a24abec32597f687
Niklas Hambüchen <mail@nh2.me>
2013-05-17 20:45:54 +0800

    Provide our own .tmLanguage file to be be able to fix smaller lexer problems.
    The fix_syntax.py script checks if the extension is .hs or .hsc, and if so sets the syntax to our syntax file.
    
    Whe start off with the original built-in .tmLanguage as of
    https://github.com/cj/sublime/blob/68a5507f11083777d71e3fb00dc5f69880750904/Haskell/Haskell.tmLanguage

---
+++ fix_syntax.py
@@ -0,0 +1,27 @@
+# Sublime's built-in Haskell lexer (Packages/Haskell/Haskell.tmLanguage)
+# is slightly broken.
+#
+# This sets the syntax of all Haskell files to our
+# SublimeHaskell/Haskell-SublimeHaskell.tmLanguage instead.
+#
+# Forked from https://gist.github.com/2940866.
+import sublime_plugin
+import os
+
+
+class DetectFileTypeCommand(sublime_plugin.EventListener):
+
+    def on_load(self, view):
+        filename = view.file_name()
+        if not filename:  # buffer has never been saved
+            return
+
+        name = os.path.basename(filename.lower())
+        if name[-3:] == ".hs" or name[-4:] == ".hsc":
+            set_our_syntax(view, filename)
+        # TODO Do we also have to fix Literate Haskell?
+
+
+def set_our_syntax(view, filename):
+    view.settings().set('syntax', 'Packages/SublimeHaskell/Haskell-SublimeHaskell.tmLanguage')
+    print "Switched syntax to SublimeHaskell's fixed Haskell syntax: " + filename
