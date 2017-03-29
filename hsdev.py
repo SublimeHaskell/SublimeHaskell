583c41ef4d376f5bc794b4284a153af3ad4bc2b9
B. Scott Michel <bscottm@ieee.org>
2017-03-28 18:12:19 -0700

    more hsdev refactoring
    Move result parsing, encoding to its own module.

--- hsdev.py
+++ hsdev.py
@@ -15,6 +15,7 @@ import sublime
 
 import SublimeHaskell.symbols as symbols
 import SublimeHaskell.sublime_haskell_common as Common
+import SublimeHaskell.hsdev_refactor.result_parse as ResultParse
 import SublimeHaskell.internals.logging as Logging
 import SublimeHaskell.internals.locked_object as LockedObject
 import SublimeHaskell.internals.proc_helper as ProcHelper
@@ -34,6 +35,7 @@ def concat_args(args):
 def hsdev_version():
     try:
         exit_code, out, err = ProcHelper.ProcHelper.run_process(['hsdev', 'version'])
+        retval = None
         if exit_code == 0:
             m = re.match(r'(?P<major>\d+)\.(?P<minor>\d+)\.(?P<revision>\d+)\.(?P<build>\d+)', out)
             if m:
@@ -41,261 +43,24 @@ def hsdev_version():
                 minor = int(m.group('minor'))
                 revision = int(m.group('revision'))
                 build = int(m.group('build'))
-                return [major, minor, revision, build]
-        # fall through
-    except FileNotFoundError:
-        pass
-
-    return None
+                retval = [major, minor, revision, build]
+    except:
+        Logging.log('Could not get hsdev version, see console window traceback', Logging.LOG_ERROR)
+        print(traceback.format_exc())
+    finally:
+        return retval
 
 
 def show_version(ver):
     return '.'.join(map(lambda i: str(i), ver))
 
 
-def check_version(ver, minimal=[0, 0, 0, 0], maximal=None):
+def check_version(ver, minimal, maximal):
     if ver is None:
         return False
-    if ver < minimal:
-        return False
-    if maximal and ver >= maximal:
+    if ver < minimal or (maximal and ver >= maximal):
         return False
     return True
-
-
-def parse_decls(s):
-    return [parse_module_declaration(decl) for decl in s] if s is not None else []
-
-
-def parse_modules_brief(s):
-    return [parse_module_id(m) for m in s] if s is not None else []
-
-
-def get_value(dc, ks, defval=None):
-    if dc is None:
-        return defval
-    if type(ks) == list:
-        cur = dc
-        for k in ks:
-            cur = cur.get(k)
-            if cur is None:
-                return defval
-        return cur
-    else:
-        return dc.get(ks, defval)
-
-
-# 'global-db', 'user-db' or {'package-db':path}
-def parse_package_db(d, defval=None):
-    if type(d) == dict:
-        pdb = get_value(d, 'package-db')
-        return symbols.PackageDb(package_db=pdb) if pdb else defval
-    if d == 'global-db':
-        return symbols.PackageDb(global_db=True)
-    if d == 'user-db':
-        return symbols.PackageDb(user_db=True)
-    return defval
-
-
-def parse_position(d):
-    if not d:
-        return None
-    line = get_value(d, 'line')
-    column = get_value(d, 'column')
-    if line is not None and column is not None:
-        return symbols.Position(line, column)
-    return None
-
-
-def parse_region(d):
-    if not d:
-        return None
-    start = parse_position(d.get('from'))
-    end = parse_position(d.get('to'))
-    if start is not None and end is not None:
-        return symbols.Region(start, end)
-    return None
-
-
-def parse_location(d):
-    loc = symbols.Location(
-        get_value(d, 'file'),
-        get_value(d, 'project'))
-    if not loc.is_null():
-        return loc
-    loc = symbols.InstalledLocation(
-        symbols.parse_package(get_value(d, 'package')),
-        parse_package_db(get_value(d, 'db')))
-    if not loc.is_null():
-        return loc
-    loc = symbols.OtherLocation(
-        get_value(d, 'source'))
-    if not loc.is_null():
-        return loc
-    return None
-
-
-def parse_import(d):
-    if not d:
-        return None
-    return symbols.Import(d['name'], d['qualified'], d.get('as'), parse_position(d.get('pos')))
-
-
-def parse_module_id(d):
-    if d is None:
-        return None
-    return symbols.Module(
-        d['name'],
-        [], [], {},
-        parse_location(d.get('location')))
-
-
-def parse_declaration(decl):
-    try:
-        what = decl['decl']['what']
-        docs = decl.get('docs')
-        name = decl['name']
-        pos = parse_position(decl.get('pos'))
-        imported = []
-        if 'imported' in decl and decl['imported']:
-            imported = [parse_import(d) for d in decl['imported']]
-        defined = None
-        if 'defined' in decl and decl['defined']:
-            defined = parse_module_id(decl['defined'])
-
-        the_decl = decl['decl']
-        decl_info = the_decl.get('info')
-
-        if what == 'function':
-            return symbols.Function(name, the_decl.get('type'), docs, imported, defined, pos)
-        else:
-            decl_ctx = decl_info.get('ctx')
-            decl_args = decl_info.get('args', [])
-            decl_def = decl_info.get('def')
-
-            if what == 'type':
-                return symbols.Type(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
-            elif what == 'newtype':
-                return symbols.Newtype(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
-            elif what == 'data':
-                return symbols.Data(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
-            elif what == 'class':
-                return symbols.Class(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
-            else:
-                return None
-    except:
-        Logging.log('Error pasring declaration, see console window traceback', Logging.LOG_ERROR)
-        print(traceback.format_exc())
-        return None
-
-
-def parse_declarations(decls):
-    return list(map(parse_declaration, decls)) if decls is not None else []
-
-
-def parse_module_declaration(d, parse_module_info=True):
-    try:
-        m = None
-        if 'module-id' in d and parse_module_info:
-            m = parse_module_id(d['module-id'])
-
-        # loc = parse_location(d['module-id'].get('location'))
-        decl = parse_declaration(d['declaration'])
-        if decl:
-            decl.module = m
-            return decl
-        else:
-            return None
-    except:
-        return None
-
-
-def parse_module(d):
-    if d is None:
-        return None
-    return symbols.Module(
-        d['name'],
-        d.get('exports'),
-        [parse_import(i) for i in d['imports']] if 'imports' in d else [],
-        dict((decl['name'], parse_declaration(decl)) for decl in d['declarations']) if 'declarations' in d else {},
-        parse_location(d.get('location')))
-
-
-def parse_modules(ds):
-    if ds is None:
-        return None
-    return [parse_module(d) for d in ds]
-
-
-def parse_cabal_package(d):
-    if d is None:
-        return None
-    return symbols.CabalPackage(
-        d['name'],
-        d.get('synopsis'),
-        d.get('default-version'),
-        d.get('installed-versions'),
-        d.get('homepage'),
-        d.get('license'))
-
-
-def parse_corrections(d):
-    return list(map(parse_correction, d)) if d is not None else d
-
-
-def parse_correction(d):
-    return symbols.Correction(d['source']['file']
-                              , d['level']
-                              , d['note']['message']
-                              , parse_corrector(d['note']['corrector'])
-                              , parse_region(d.get('region')))
-
-
-def parse_corrector(d):
-    return symbols.Corrector(parse_region(d['region']), d['contents'])
-
-
-def encode_corrections(corrections):
-    return list(map(encode_correction, corrections))
-
-
-def encode_correction(c):
-    return {
-        'source': {
-            'project': None,
-            'file': c.file},
-        'level': c.level,
-        'note': {
-            'corrector': encode_corrector(c.corrector),
-            'message': c.message},
-        'region': {
-            'from': encode_position(c.corrector.start.from_zero_based()),
-            'to': encode_position(c.corrector.end.from_zero_based())}
-        }
-
-
-def encode_corrector(c):
-    return {
-        'region': {
-            'from': encode_position(c.start),
-            'to': encode_position(c.end)},
-        'contents': c.contents}
-
-
-def encode_position(p):
-    return {
-        'line': p.line,
-        'column': p.column}
-
-
-def encode_package_db(db):
-    if db.user_db:
-        return 'user-db'
-    if db.global_db:
-        return 'global-db'
-    if db.package_db:
-        return {'package-db': db.package_db}
-    return None
 
 
 def reconnect_function(fn):
@@ -424,8 +189,10 @@ def call_callback(fn, *args, **kwargs):
     try:
         if fn is not None:
             fn(*args, **kwargs)
-    except Exception as e:
-        Logging.log("callback '{0}' throws exception: {1}".format(name or '<unnamed>', e))
+    except:
+        Logging.log('callback \'{0}\' throws exception, see console window traceback'.format(name or '<unnamed>'),
+                    Logging.LOG_ERROR)
+        print(traceback.format_exc())
 
 
 def format_error_details(ds):
@@ -776,7 +543,7 @@ class HsDev(object):
         if cabal:
             fs.append({'cabal': 'cabal'})
         if db:
-            fs.append({'db': encode_package_db(db)})
+            fs.append({'db': ResultParse.encode_package_db(db)})
         if package:
             fs.append({'package': package})
         if source:
@@ -784,7 +551,7 @@ class HsDev(object):
         if standalone:
             fs.append('standalone')
 
-        return cmd('modules', {'filters': fs}, parse_modules_brief)
+        return cmd('modules', {'filters': fs}, ResultParse.parse_modules_brief)
 
     @list_command
     def list_packages(self):
@@ -814,7 +581,7 @@ class HsDev(object):
         if cabal:
             fs.append({'cabal': 'cabal'})
         if db:
-            fs.append({'db': encode_package_db(db)})
+            fs.append({'db': ResultParse.encode_package_db(db)})
         if package:
             fs.append({'package': package})
         if source:
@@ -822,7 +589,7 @@ class HsDev(object):
         if standalone:
             fs.append('standalone')
 
-        return cmd('symbol', {'query': q, 'filters': fs, 'locals': locals}, parse_decls)
+        return cmd('symbol', {'query': q, 'filters': fs, 'locals': locals}, ResultParse.parse_decls)
 
     @command
     def module(self, input="", search_type='prefix', project=None, file=None, module=None, deps=None, sandbox=None,
@@ -843,7 +610,7 @@ class HsDev(object):
         if cabal:
             fs.append({'cabal': 'cabal'})
         if db:
-            fs.append({'db': encode_package_db(db)})
+            fs.append({'db': ResultParse.encode_package_db(db)})
         if package:
             fs.append({'package': package})
         if source:
@@ -851,11 +618,11 @@ class HsDev(object):
         if standalone:
             fs.append('standalone')
 
-        return cmd('module', {'query': q, 'filters': fs}, parse_modules)
+        return cmd('module', {'query': q, 'filters': fs}, ResultParse.parse_modules)
 
     @command
     def resolve(self, file, exports=False):
-        return cmd('resolve', {'file': file, 'exports': exports}, parse_module)
+        return cmd('resolve', {'file': file, 'exports': exports}, ResultParse.parse_module)
 
     @command
     def project(self, project=None, path=None):
@@ -867,32 +634,32 @@ class HsDev(object):
 
     @list_command
     def lookup(self, name, file):
-        return cmd('lookup', {'name': name, 'file': file}, parse_decls)
+        return cmd('lookup', {'name': name, 'file': file}, ResultParse.parse_decls)
 
     @list_command
     def whois(self, name, file):
-        return cmd('whois', {'name': name, 'file': file}, parse_declarations)
+        return cmd('whois', {'name': name, 'file': file}, ResultParse.parse_declarations)
 
     @list_command
     def scope_modules(self, file, input='', search_type='prefix'):
-        return cmd('scope modules', {'query': {'input': input, 'type': search_type}, 'file': file}, parse_modules_brief)
+        return cmd('scope modules', {'query': {'input': input, 'type': search_type}, 'file': file}, ResultParse.parse_modules_brief)
 
     @list_command
     def scope(self, file, input='', search_type='prefix', global_scope=False):
         return cmd('scope', {'query': {'input': input, 'type': search_type},
-                             'global': global_scope, 'file': file}, parse_declarations)
+                             'global': global_scope, 'file': file}, ResultParse.parse_declarations)
 
     @list_command
     def complete(self, input, file, wide=False):
-        return cmd('complete', {'prefix': input, 'wide': wide, 'file': file}, parse_declarations)
+        return cmd('complete', {'prefix': input, 'wide': wide, 'file': file}, ResultParse.parse_declarations)
 
     @list_command
     def hayoo(self, query, page=None, pages=None):
-        return cmd('hayoo', {'query': query, 'page': page or 0, 'pages': pages or 1}, parse_decls)
+        return cmd('hayoo', {'query': query, 'page': page or 0, 'pages': pages or 1}, ResultParse.parse_decls)
 
     @list_command
     def cabal_list(self, packages):
-        cmd('cabal list', {'packages': packages}, lambda r: [parse_cabal_package(s) for s in r] if r else None)
+        cmd('cabal list', {'packages': packages}, lambda r: [ResultParse.parse_cabal_package(s) for s in r] if r else None)
 
     @list_command
     def lint(self, files=[], contents={}, hlint=[]):
@@ -933,11 +700,11 @@ class HsDev(object):
 
     @list_command
     def autofix_show(self, messages):
-        return cmd('autofix show', {'messages': messages}, parse_corrections)
+        return cmd('autofix show', {'messages': messages}, ResultParse.parse_corrections)
 
     @list_command
     def autofix_fix(self, messages, rest=[], pure=False):
-        return cmd('autofix fix', {'messages': messages, 'rest': rest, 'pure': pure}, parse_corrections)
+        return cmd('autofix fix', {'messages': messages, 'rest': rest, 'pure': pure}, ResultParse.parse_corrections)
 
     @list_command
     def ghc_eval(self, exprs, file=None, source=None):
---
+++ hsdev_refactor/result_parse.py
@@ -0,0 +1,234 @@
+
+import traceback
+
+import SublimeHaskell.internals.logging as Logging
+import SublimeHaskell.symbols as symbols
+
+def parse_decls(decls):
+    return [parse_module_declaration(decl) for decl in decls] if decls is not None else []
+
+
+def parse_modules_brief(mods):
+    return [parse_module_id(m) for m in mods] if mods is not None else []
+
+
+def get_value(dc, ks, defval=None):
+    if dc is None:
+        return defval
+    if isinstance(ks, list):
+        cur = dc
+        for k in ks:
+            cur = cur.get(k)
+            if cur is None:
+                return defval
+        return cur
+    else:
+        return dc.get(ks, defval)
+
+
+# 'global-db', 'user-db' or {'package-db':path}
+def parse_package_db(d, defval=None):
+    if type(d) == dict:
+        pdb = get_value(d, 'package-db')
+        return symbols.PackageDb(package_db=pdb) if pdb else defval
+    if d == 'global-db':
+        return symbols.PackageDb(global_db=True)
+    if d == 'user-db':
+        return symbols.PackageDb(user_db=True)
+    return defval
+
+
+def parse_position(d):
+    if not d:
+        return None
+    line = get_value(d, 'line')
+    column = get_value(d, 'column')
+    if line is not None and column is not None:
+        return symbols.Position(line, column)
+    return None
+
+
+def parse_region(d):
+    if not d:
+        return None
+    start = parse_position(d.get('from'))
+    end = parse_position(d.get('to'))
+    if start is not None and end is not None:
+        return symbols.Region(start, end)
+    return None
+
+
+def parse_location(d):
+    loc = symbols.Location(
+        get_value(d, 'file'),
+        get_value(d, 'project'))
+    if not loc.is_null():
+        return loc
+    loc = symbols.InstalledLocation(
+        symbols.parse_package(get_value(d, 'package')),
+        parse_package_db(get_value(d, 'db')))
+    if not loc.is_null():
+        return loc
+    loc = symbols.OtherLocation(
+        get_value(d, 'source'))
+    if not loc.is_null():
+        return loc
+    return None
+
+
+def parse_import(d):
+    if not d:
+        return None
+    return symbols.Import(d['name'], d['qualified'], d.get('as'), parse_position(d.get('pos')))
+
+
+def parse_module_id(d):
+    if d is None:
+        return None
+    return symbols.Module(
+        d['name'],
+        [], [], {},
+        parse_location(d.get('location')))
+
+
+def parse_declaration(decl):
+    try:
+        what = decl['decl']['what']
+        docs = decl.get('docs')
+        name = decl['name']
+        pos = parse_position(decl.get('pos'))
+        imported = []
+        if 'imported' in decl and decl['imported']:
+            imported = [parse_import(d) for d in decl['imported']]
+        defined = None
+        if 'defined' in decl and decl['defined']:
+            defined = parse_module_id(decl['defined'])
+
+        the_decl = decl['decl']
+        decl_info = the_decl.get('info')
+
+        if what == 'function':
+            return symbols.Function(name, the_decl.get('type'), docs, imported, defined, pos)
+        else:
+            decl_ctx = decl_info.get('ctx')
+            decl_args = decl_info.get('args', [])
+            decl_def = decl_info.get('def')
+
+            if what == 'type':
+                return symbols.Type(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
+            elif what == 'newtype':
+                return symbols.Newtype(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
+            elif what == 'data':
+                return symbols.Data(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
+            elif what == 'class':
+                return symbols.Class(name, decl_ctx, decl_args, decl_def, docs, imported, defined, pos)
+            else:
+                return None
+    except:
+        Logging.log('Error pasring declaration, see console window traceback', Logging.LOG_ERROR)
+        print(traceback.format_exc())
+        return None
+
+
+def parse_declarations(decls):
+    return list(map(parse_declaration, decls)) if decls is not None else []
+
+
+def parse_module_declaration(d, parse_module_info=True):
+    try:
+        m = None
+        if 'module-id' in d and parse_module_info:
+            m = parse_module_id(d['module-id'])
+
+        # loc = parse_location(d['module-id'].get('location'))
+        decl = parse_declaration(d['declaration'])
+        if decl:
+            decl.module = m
+            return decl
+        else:
+            return None
+    except:
+        return None
+
+
+def parse_module(d):
+    if d is None:
+        return None
+    return symbols.Module(
+        d['name'],
+        d.get('exports'),
+        [parse_import(i) for i in d['imports']] if 'imports' in d else [],
+        dict((decl['name'], parse_declaration(decl)) for decl in d['declarations']) if 'declarations' in d else {},
+        parse_location(d.get('location')))
+
+
+def parse_modules(ds):
+    if ds is None:
+        return None
+    return [parse_module(d) for d in ds]
+
+
+def parse_cabal_package(d):
+    if d is None:
+        return None
+    return symbols.CabalPackage(d['name'], d.get('synopsis'), d.get('default-version'), d.get('installed-versions'),
+                                d.get('homepage'), d.get('license'))
+
+
+def parse_corrections(d):
+    return list(map(parse_correction, d)) if d is not None else d
+
+
+def parse_correction(d):
+    return symbols.Correction(d['source']['file']
+                              , d['level']
+                              , d['note']['message']
+                              , parse_corrector(d['note']['corrector'])
+                              , parse_region(d.get('region')))
+
+
+def parse_corrector(d):
+    return symbols.Corrector(parse_region(d['region']), d['contents'])
+
+
+def encode_corrections(corrections):
+    return list(map(encode_correction, corrections))
+
+
+def encode_correction(c):
+    return {
+        'source': {
+            'project': None,
+            'file': c.file},
+        'level': c.level,
+        'note': {
+            'corrector': encode_corrector(c.corrector),
+            'message': c.message},
+        'region': {
+            'from': encode_position(c.corrector.start.from_zero_based()),
+            'to': encode_position(c.corrector.end.from_zero_based())}
+        }
+
+
+def encode_corrector(c):
+    return {
+        'region': {
+            'from': encode_position(c.start),
+            'to': encode_position(c.end)},
+        'contents': c.contents}
+
+
+def encode_position(p):
+    return {
+        'line': p.line,
+        'column': p.column}
+
+
+def encode_package_db(db):
+    if db.user_db:
+        return 'user-db'
+    if db.global_db:
+        return 'global-db'
+    if db.package_db:
+        return {'package-db': db.package_db}
+    return None
